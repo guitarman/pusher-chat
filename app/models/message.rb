efcbf9871c30354f4645317ca2b94937ca95f55f
jantoth <jant@boxdice.com.au>
2013-10-21 15:51:09 +0200

    many-to-many association between user and message

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,7 @@
 class Message < ActiveRecord::Base
   belongs_to :channel
-  belongs_to :sender, :foreign_key => 'created_by'
+  belongs_to :sender, :foreign_key => 'created_by', :class_name => "User"
+
+  has_many :message_views
+  has_many :users, :through => :message_views
 end
