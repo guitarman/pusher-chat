6388d45f750f4233d646e548944fea3837303566
jantoth <jant@boxdice.com.au>
2013-10-21 15:39:47 +0200

    specify class_name in sender association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   has_many :sent_messages, :foreign_key => 'created_by', :class_name => "Message"
 
+  has_many :message_views
+  has_many :received_messages, :through => :message_views
+
   def self.current(user_id)
     User.find_by id: user_id
   end
