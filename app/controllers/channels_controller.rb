bdee634601da0369f36e1411ff36c9fa1ad4a498
jantoth <jant@boxdice.com.au>
2013-10-22 17:12:09 +0200

    create subscriptions action

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -5,6 +5,17 @@ class ChannelsController < ApplicationController
     respond_with Channel.find_or_create_by(permitted_params)
   end
 
+  def create_subscriptions
+    channel = Channel.find_by(name: params[:channelName])
+    logger.error params[:subscribers].class
+    params[:subscribers].each do |subscriber_id|
+      subscriber = User.find(subscriber_id)
+      logger.error Subscription.new(channel: channel, user: subscriber).to_yaml
+    end
+
+    render text: 'ok', status: 200
+  end
+
   private
 
   def permitted_params
